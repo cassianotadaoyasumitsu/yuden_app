80730c7f73e0478f31b44ac529dc2fc7a8ef888e
cassiano yasumitsu <cassiano@usuarioincomum.com>
2020-08-23 08:13:02 +0900

    out

--- app/controllers/puntos_controller.rb
+++ app/controllers/puntos_controller.rb
@@ -20,25 +20,38 @@ class PuntosController < ApplicationController
     @punto = Punto.new(punto_params)
     @punto.user = @user
     if @punto.save
-      redirect_to punto_path(@punto)
+      redirect_to new_punto_path(@punto)
     else
       flash.alert = "No punto"
       render :new
     end
   end
 
-  private
-
-  def find_user
-    @user = User.find(current_user.id)
+  def edit
   end
 
-  def punto_params
-    params.require(:punto).permit(
-      :in, :out, :date, :note,
-      :active, :weekend, :night_shift,
-      :day, :night, :day_off
-      )
+  def update
+    @punto = current_user.puntos.last
+    if @punto.in
+     @punto.update(punto_params)
+     redirect_to authenticated_root_path
+   else
+    render :edit
   end
+end
+
+private
+
+def find_user
+  @user = User.find(current_user.id)
+end
+
+def punto_params
+  params.require(:punto).permit(
+    :in, :out, :date, :note,
+    :active, :weekend, :night_shift,
+    :day, :night, :day_off
+    )
+end
 end
 
